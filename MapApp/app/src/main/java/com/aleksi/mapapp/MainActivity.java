161ed8b61b66761e3d4746eca70ca01e46952696
Aleksi Moilanen <DC\ttv16aleksim@ttvmint13.dc.lab>
2018-09-25 15:22:37 +0300

    Last location and show me in the map features

--- MapApp/app/src/main/java/com/aleksi/mapapp/MainActivity.java
+++ MapApp/app/src/main/java/com/aleksi/mapapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.aleksi.mapapp;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -17,6 +18,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT); //Sets screen rotation locked
+    }
+
+
+    public void openMapMe(View view){
+        Intent intent = new Intent(this, MapsActivity.class);
+
+        Bundle extras = new Bundle();
+        extras.putChar("OPERATOR", 'M');
+
+        intent.putExtras(extras);
+        startActivity(intent);
+    }
+
+    public void openMapPos(View view){
+        Intent intent = new Intent(this, MapsActivity.class);
+
+        Bundle extras = new Bundle();
+        extras.putChar("OPERATOR", 'L');
+
+        intent.putExtras(extras);
+        startActivity(intent);
     }
 
     public void openMapCoord(View view){
@@ -26,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
         EditText CoordLong = (EditText) findViewById(R.id.CoordLong);
 
 
-        //if(CoordLat.getText().toString().isEmpty() ||CoordLong.getText().toString().isEmpty()){
+        if(!CoordLat.getText().toString().isEmpty() || !CoordLong.getText().toString().isEmpty()){
             Double latCoord = Double.parseDouble(CoordLat.getText().toString());
             Double longCoord = Double.parseDouble(CoordLong.getText().toString());
 
@@ -35,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
             if (longCoord < 180 && longCoord > -180) {
                 if (latCoord < 85 && latCoord > -85) {
                     Bundle extras = new Bundle();
-                    extras.putString("OPERATOR", "C");
+                    extras.putChar("OPERATOR", 'C');
                     extras.putDouble("CoordLat", latCoord);
                     extras.putDouble("CoordLong", longCoord);
 
@@ -47,9 +70,11 @@ public class MainActivity extends AppCompatActivity {
             } else {
                 Toast.makeText(this, "Long value have to be between 180 and -180", Toast.LENGTH_LONG).show();
             }
-        /*} else {
+        } else {
             Toast.makeText(this, "Insert both values", Toast.LENGTH_LONG).show();
             Log.d("COORD", CoordLat.getText().toString() + CoordLong.getText().toString());
-        }*/
+        }
     }
+
+
 }
--- MapApp/app/src/main/java/com/aleksi/mapapp/MapsActivity.java
+++ MapApp/app/src/main/java/com/aleksi/mapapp/MapsActivity.java
@@ -1,9 +1,17 @@
 package com.aleksi.mapapp;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -17,11 +25,18 @@ import java.util.ArrayList;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+
+    public char operator;
+
     public double CoordLat = 64.222;
     public double CoordLong = 27.727;
 
+    private static final int MY_PERMISSION_REQUEST_FINE_LOCATION = 0;
+    private static final int MY_PERMISSION_REQUEST_COARSE_LOCATION = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
@@ -30,17 +45,19 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
 
         Bundle extras = getIntent().getExtras();
-        String operator = extras.getString("OPERATOR");
+        operator = extras.getChar("OPERATOR");
 
-        Log.d("COORD", operator);
-
-        //if(operator == "C")
-        //{
+        if (operator == 'C') {
+            Log.d("DEBUG_STNA", "Coordinates");
             CoordLat = extras.getDouble("CoordLat");
             CoordLong = extras.getDouble("CoordLong");
-        //}
-    }
 
+        } else if (operator == 'L') {
+            Log.d("DEBUG_STNA", "Location");
+        } else if (operator == 'L') {
+            Log.d("DEBUG_STNA", "Me on the map");
+        }
+    }
 
     /**
      * Manipulates the map once available.
@@ -55,9 +72,70 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(CoordLat, CoordLong);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSION_REQUEST_FINE_LOCATION);
+                Log.d("DEBUG_STNA", "Fine location permission");
+            }
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_COARSE_LOCATION)) {
+
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, MY_PERMISSION_REQUEST_COARSE_LOCATION);
+                Log.d("DEBUG_STNA", "Coarse location permission");
+            }
+
+            return;
+        } else {
+            Log.d("DEBUG_STNA", "All permissions ok!");
+        }
+
+        if(operator == 'L') {
+            LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+            Criteria criteria = new Criteria();
+            Location location = locationManager.getLastKnownLocation(locationManager.getBestProvider(criteria, true));
+
+            CoordLat = location.getLatitude();
+            CoordLong = location.getLongitude();
+        } else if(operator == 'M') {
+            mMap.setMyLocationEnabled(true);
+        }
+
+        if(operator == 'L' || operator == 'C'){
+            LatLng position = new LatLng(CoordLat, CoordLong);
+            mMap.addMarker(new MarkerOptions().position(position).title("Marker in Position"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(position));
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
+        switch (requestCode) {
+            case MY_PERMISSION_REQUEST_FINE_LOCATION:{
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+                } else {
+
+                }
+                return;
+            }
+            case MY_PERMISSION_REQUEST_COARSE_LOCATION:{
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+                } else {
+
+                }
+                return;
+            }
+        }
     }
 }
